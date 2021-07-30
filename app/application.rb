d50d3b3d1fd449c0e6b6809a2ba2826f397b91cc
charcodes24 <charcodes24@gmail.com>
2021-07-30 13:06:37 -0700

    GET response for hikes .

--- app/application.rb
+++ app/application.rb
@@ -4,7 +4,25 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    if req.path.match(/national_parks/) && req.get?
+    if req.path.match('/national_parks/hikes/') && req.get?
+      name = req.path.split("/national_parks/hikes/").last
+      new_name = name.gsub!(/[20%]/, " ").squeeze(' ')
+      park = NationalPark.find_by_name(new_name)
+      hikes = park.hikes
+
+      display_hikes = hikes.map do |t|
+        {
+          id: t.id, 
+          name: t.name, 
+          distance: t.distance, 
+          difficulty: t.difficulty, 
+          note: t.note
+        }
+      end
+
+      return [200, { 'Content-Type' => 'application/json' }, [ {hikes: display_hikes, message: "request successful"}.to_json ]]
+
+    elsif req.path.match(/national_parks/) && req.get?
 
       return [200, { 'Content-Type' => 'application/json' }, [ {national_parks: NationalPark.render_all, message: "request successful"}.to_json ]]
 
@@ -28,7 +46,7 @@ class Application
       else
         return [422, { 'Content-Type' => 'application/json' }, [ {:error => "unable to delete task"}.to_json ]]
       end
-      
+
     else
       resp.write "Path Not Found"
 
