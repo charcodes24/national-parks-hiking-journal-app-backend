4d2ebf38da9338ee3d45bef5ca63657a8d59b9d7
charcodes24 <charcodes24@gmail.com>
2021-07-30 11:27:37 -0700

    POST request response

--- app/application.rb
+++ app/application.rb
@@ -4,9 +4,19 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    if req.path.match('/national_parks/') && req.get?
+    if req.path.match(/national_parks/) && req.get?
 
       return [200, { 'Content-Type' => 'application/json' }, [ {national_parks: NationalPark.render_all, message: "request successful"}.to_json ]]
+
+    elsif req.path.match(/national_parks/) && req.post?
+
+      newPark = NationalPark.new(JSON.parse(req.body.read))
+
+      if newPark.save
+        return [200, { 'Content-Type' => 'application/json' }, [ {:park => newPark, :message => "park successfully added"}.to_json ]]
+      else 
+        return [422, { 'Content-Type' => 'application/json' }, [ {:error => "failed to create"}.to_json ]]
+      end
 
     else
       resp.write "Path Not Found"
